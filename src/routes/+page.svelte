aea95f89ec43e27325635a370eed87c90d9a8c09
Charles <cchester1@angelo.edu>
2024-06-25 15:16:24 -0500

    category design coming along. Need to either write quick json to store album uri's and category names or write a backend to store and serve that data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,15 +10,20 @@
       })
 </script>
 
-<div class="">
+<div class="h-full flex flex-col">
       <!-- categories container
       add new category button in top left
       category cards container
 
       modal that is activated if new category button clicked
       save button to save new category or cancel button to delete modal state and close modal -->
-      <div>
-            <button>Add Category</button>
+      <div class="m-7">
+            <button class="bg-transparent hover:bg-slate-900 text-slate-900 font-semibold hover:text-slate-100 py-2 px-4 border border-slate-900 hover:border-transparent rounded" style="font-family: Playwrite US Trad, cursive; font-weight: 400; font-style: normal;"
+            >
+                  Add Category
+            </button>
       </div>
-      <div>Albums</div>
+      <div class="h-full flex items-center justify-center">
+            Categories
+      </div>
 </div>
