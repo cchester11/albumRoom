e2bd6068870cfd2fa1bb3e654b7d7886417d7160
Charles <cchester1@angelo.edu>
2024-06-26 08:48:33 -0500

    modal up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { onMount } from 'svelte';
+	import { writable } from 'svelte/store';
 	import { getSpotifyAuth, getAudioFeatures_Album } from '$lib';
 
 	onMount(() => {
@@ -8,6 +9,13 @@
 
 		console.log(response, album);
 	});
+
+	// Manage modal visibility
+	const showModal = writable(false);
+
+	function toggleModal() {
+		showModal.update(value => !value)
+	};
 </script>
 
 <div class="h-full flex flex-col bg-lounge bg-cover bg-center">
@@ -22,9 +30,40 @@
 			class="bg-transparent hover:bg-slate-900 text-slate-900 font-semibold hover:text-slate-100 py-2 px-4 border border-slate-900 hover:border-transparent rounded"
 			style="font-family: Playwrite US Trad, cursive; font-weight: 400; font-style: normal;"
 			id="add_category"
+			type="button"
+			on:click={toggleModal}
 		>
 			Add Category
 		</button>
 	</div>
 	<div class="h-full flex items-center justify-center">Categories</div>
+
+	<!-- Category Modal -->
+	{#if $showModal}
+	<div tabindex="-1" aria-hidden="true" class="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+		<div class="bg-white p-6 rounded-lg shadow-lg w-1/3 h-1/3">
+			<div>
+				<h1 class="text-xl font-semibold mb-4">Category</h1>
+			</div>
+			<div class="mb-4">
+				<label for="category" class="block text-gray-700 mb-2">Category</label>
+				<input type="text" name="category" id="category" placeholder="Jazz" required class="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"/>
+			</div>
+			<div class="flex justify-end">
+				<button
+					class="bg-slate-900 hover:bg-slate-700 text-white font-semibold py-2 px-4 rounded mr-2"
+					on:click={toggleModal}
+				>
+					Cancel
+				</button>
+				<button
+					class="bg-lime-700 hover:bg-lime-900 text-white font-semibold py-2 px-4 rounded"
+					on:click={toggleModal}
+				>
+					Save
+				</button>
+			</div>
+		</div>
+	</div>
+	{/if}
 </div>
