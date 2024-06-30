7eed46caef3417f2949bcc9dfab0e78adce3d4ab
Charles <cchester1@angelo.edu>
2024-06-30 11:14:23 -0500

    layout fixed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,60 +1,51 @@
 <script>
-      import '../app.css';
+	import '../app.css';
 </script>
 
-<div class="w-full h-full flex flex-col justify-between">
-	<header class="bg-slate-900 font-extralight text-4xl text-slate-100">
-		<div class="flex items-center justify-center h-24">
-			<nav class="h-full flex items-center justify-center">
-				<h1 style="font-family: Playwrite US Trad, cursive; font-weight: 400; font-style: normal;">
-					<span>
-						<a href="/">
-							Album Room
-						</a>
-					</span>
-				</h1>
-			</nav>
-		</div>
+<div class="min-h-screen flex flex-col">
+	<header class="bg-slate-900 text-slate-100 h-24 flex items-center justify-center">
+		<nav class="h-full flex items-center justify-center">
+			<h1
+				class="font-extralight text-4xl"
+				style="font-family: 'Playwrite US Trad', cursive; font-weight: 400;"
+			>
+				<span>
+					<a href="/">Album Room</a>
+				</span>
+			</h1>
+		</nav>
 	</header>
 
-      <slot>
-      
-      </slot>
+	<main class="flex-grow">
+		<slot></slot>
+	</main>
 
-      <footer class="font-thin text-slate-100 bg-slate-900">
-		<div class="flex flex-row justify-between px-10 items-center h-36">
-			<div class="flex flex-row items-center mr-3">
-				<div class="mx-3">
-					<img
-						class="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12"
-						src="/github_neon_icon.png"
-						alt="github"
-					/>
-				</div>
-				<div class="mx-3">
-					<img
-						class="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12"
-						src="/photo_icon.png"
-						alt="instagram"
-					/>
-				</div>
-				<div class="mx-3">
-					<img
-						class="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12"
-						src="/substack.png"
-						alt="substack"
-					/>
-				</div>
-			</div>
+	<footer class="bg-slate-900 text-slate-100 h-36 flex items-center justify-between px-10">
+		<div class="flex items-center">
+			<img
+				class="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 mx-3"
+				src="/github_neon_icon.png"
+				alt="github"
+			/>
+			<img
+				class="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 mx-3"
+				src="/photo_icon.png"
+				alt="instagram"
+			/>
+			<img
+				class="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 mx-3"
+				src="/substack.png"
+				alt="substack"
+			/>
+		</div>
 
-			<div class="text-sm ml-3">
-				<p>
-					Powered by:
-					<span>
-						<a href="https://github.com/cchester11" target="_blank">github.com/cchester11</a>
-					</span>
-				</p>
-			</div>
+		<div class="text-sm ml-3">
+			<p>
+				Powered by:
+				<span>
+					<a href="https://github.com/cchester11" target="_blank">github.com/cchester11</a>
+				</span>
+			</p>
 		</div>
 	</footer>
 </div>
